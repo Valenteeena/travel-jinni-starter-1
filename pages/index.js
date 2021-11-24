64c23eb4df7e44c298daf9fcae87b2725dc7bbf3
Olayemi Adetoun <valenteeena@gmail.com>
2021-11-24 17:03:33 +0100

    Tablet View fix

--- pages/index.js
+++ pages/index.js
@@ -11,32 +11,34 @@ export default function Home() {
 
             <div className={styles.header}>
                 <div>
-                    <p className="text-sm phone:text-xs">Remaining Days to my Trip</p>
+                    <p className="desktop:text-sm tablet:text-sm phone:text-xs">
+                        Remaining Days to my Trip
+                    </p>
 
                     <div className="flex">
-                        <div className="flex-col text-center mx-1 w-10 phone:w-6">
+                        <div className="flex-col text-center mx-1 desktop:w-10 tablet:w-8 phone:w-6">
                             <h2 className={styles.pFlexT}>10</h2>
                             <p className={styles.txtSpan}>Weeks</p>
                         </div>
-                        <div className="flex-col text-center mx-1 w-10 phone:w-6">
+                        <div className="flex-col text-center mx-1 desktop:w-10 tablet:w-8 phone:w-6">
                             <h2 className={styles.pFlexT}>06</h2>
                             <p className={styles.txtSpan}>Days</p>
                         </div>
-                        <div className="flex-col text-center mx-1 w-10 phone:w-6">
+                        <div className="flex-col text-center mx-1 desktop:w-10 tablet:w-8 phone:w-6">
                             <h2 className={styles.pFlexT}>45</h2>
                             <p className={styles.txtSpan}>Hours</p>
                         </div>
-                        <div className="flex-col text-center mx-1 w-10 phone:w-8">
+                        <div className="flex-col text-center mx-1 desktop:w-10 tablet:w-8 phone:w-8">
                             <h2 className={styles.pFlexT}>25</h2>
                             <p className={styles.txtSpan}>Minutes</p>
                         </div>
-                        <div className="flex-col text-center mx-1 w-10 phone:w-6">
+                        <div className="flex-col text-center mx-1 desktop:w-10 tablet:w-8 phone:w-6">
                             <h2 className={styles.pFlexT}>06</h2>
                             <p className={styles.txtSpan}>Seconds</p>
                         </div>
                     </div>
                 </div>
-                <div className="flex text-sm py-5 phone:py-4">
+                <div className="flex text-sm desktop:py-5 phone:py-4">
                     <p className="px-2">My Trips</p>
                     <div className="flex">
                         <p className="px-2">|</p>
@@ -59,7 +61,7 @@ export default function Home() {
             </div>
             <div className={styles.content}>
                 <div className={styles.leftSide}>
-                    <h1 className="font-bold text-xl font-serif py-1 phone:text-sm">
+                    <h1 className="font-bold desktop:text-xl tablet:text-lg font-serif py-1 phone:text-sm">
                         Six Days In Dubai
                     </h1>
                     <ul>
@@ -76,23 +78,23 @@ export default function Home() {
                 </div>
                 <div>
                     <div className={styles.rightSide}>
-                        <div className="rounded-full w-20 h-20 p-2 flex items-center justify-center bg-white phone:w-12 phone:h-12">
+                        <div className="rounded-full desktop:w-20 desktop:h-20 tablet:w-16 tablet:h-16 p-2 flex items-center justify-center bg-white phone:w-12 phone:h-12">
                             <img src="/logo.png" alt="logo" className="w-18 h-18 rounded-full " />
                         </div>
                         <div className="ml-4 text-center border border-white">
-                            <h1 className="head-t font-bold font-mono tracking-wide px-3 phone:text-2xl">
+                            <h1 className="head-t font-bold desktop:text-5xl tablet:text-4xl font-mono tracking-wide px-3 phone:text-2xl">
                                 HILTON HOTEL
                             </h1>
-                            <h2 className="bg-white text-black text-xl pb-1 phone:text-sm">
+                            <h2 className="bg-white text-black desktop:text-xl tablet:text-lg pb-1 phone:text-sm">
                                 POWERED BY TRAVEL JINNI
                             </h2>
                         </div>
                     </div>
                     <div className={`div-w blur-filter ${styles.conBody}`}>
-                        <h1 className="text-5xl font-serif font-bold pb-5 px-4 phone:text-3xl">
+                        <h1 className="desktop:text-5xl tablet:text-4xl font-serif font-bold pb-5 px-4 phone:text-3xl">
                             Your Trip Awaits You!
                         </h1>
-                        <p className="text-sm pb-5 phone:text-xs">
+                        <p className="desktop:text-sm tablet:text-sm pb-5 phone:text-xs">
                             We have taken some time to help plan your trip to make it a memmorable
                             one. Access your calender for even more things you can do on your trip.
                         </p>
